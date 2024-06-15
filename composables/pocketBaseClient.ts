5e7063975c8db6a6c2e748ba51afea8be9824f3f
denver-code <csigorek@gmail.com>
2024-06-15 15:19:20 +0100

    apiurl fix

--- composables/pocketBaseClient.ts
+++ composables/pocketBaseClient.ts
@@ -1,7 +1,8 @@
 import PocketBase from 'pocketbase';
-const pb = new PocketBase('http://192.168.1.207:8090/');
+const pb = new PocketBase('https://sapi.savenko.tech/');
 
-// 'https://sapi.savenko.tech/'
+// 
+// 'http://192.168.1.207:8090/'
 export const usePocketBaseClient = () => {
   return pb as PocketBase;
 }
